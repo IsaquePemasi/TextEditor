923c948ba1a05242508591defeb55aab1ea4f23d
IsaquePemasi <ADSisaque@gmail.com>
2023-03-21 18:17:27 -0300

    add

--- Program.cs
+++ Program.cs
@@ -51,6 +51,18 @@ namespace TextEditor
 
             Console.Write(text);
         }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo? ");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+        }
     }
 }
 
